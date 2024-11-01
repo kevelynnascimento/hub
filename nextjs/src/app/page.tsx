3b2c4c78b7f3c46ce3d2c4b39fa64208c4e2c9de
Kevelyn Nascimento <kevelynbarbosa@gmail.com>
2024-11-01 20:16:37 -0300

    update nextjs project

---
+++ nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3x1 font-bold">Home</h1>
+      </div>
+    </section>
+  );
+}
